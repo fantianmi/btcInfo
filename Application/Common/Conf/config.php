cfe6cff2512586589a2b827e122b6fb93d1ca57e
jackmao <fantianmi@163.com>
2019-10-08 15:13:34 +0800

    修改默认首页

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/10/8
+ * Time: 15:12
+ */
+return array(
+    'DEFAULT_MODULE' => 'Home', // 默认模块
+    'DEFAULT_CONTROLLER' => 'Analysis', // 默认控制器名称
+    'DEFAULT_ACTION' => 'otc', // 默认操作名称
+);
